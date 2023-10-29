c3535ac3f5790a7293e78fcfed00e45ec77c77bf
ragul0427 <ragulhp2704@gmail.com>
2023-10-29 22:23:06 +0530

    rtbfhhr

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,63 +1,85 @@
-const knex = require("../db");
+  const knex = require("../db");
 
 
-const createUser = async (req, res) => {
-  console.log(req.body,"Wejb")
-  knex("users")
-    .insert({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      dob: req.body.dob,
-      address: req.body.address,
-    })
-    .then(() => {
-      res.json({
-        message: `Book \ ${req.body.FirstName} and ${req.body.LastName} and ${req.body.Address} and ${req.body.DOB} created`,
-      });
-    })
-    .catch((err) => {
-      res.send({
-        message: `there are error while creating a user ${req.body.FirstName} ${req.body.LastName}`,
-      });
-    });
-};
-
-const getUser = async (req, res) => {
-  knex
-    .select("*")
-    .from("users")
-    .then((data) => res.json({ data: data }))
-    .catch((err) => console.log(err));
-};
-
-const updateUser = async (req, res) => {
+  const validateUserFields = (fields) => {
+    const requiredFields = ['firstName', 'lastName', 'dob', 'address'];
+    const emptyFields = [];
   
-  knex("users")
-    .where("id", req.params.id)
-    .update({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      dob: req.body.dob,
-      address: req.body.address,
-    })
-    .then((data) => {
-      res.send({message:"users updated successfully"})
-    })
-    .catch((err) => {
-      res.send({message:"erro while updating",err})
-    });
-};
+    for (const field of requiredFields) {
+      if (!fields[field]) {
+        emptyFields.push(field);
+      }
+    }
+  
+    if (emptyFields.length > 0) {
+      return `${emptyFields.join(', ')} ${emptyFields.length > 1 ? 'are' : 'is'} required`;
+    }
+  
+    return null;
+  };
 
-const deleteUser = async (req, res) => {
-  knex("users")
-    .where("id", req.body.id)
-    .del()
-    .then(() => {
-      res.json({ message: `Book ${req.body.id} deleted.` });
-    })
-    .catch((err) => {
+  const createUser = async (req, res) => {
+    try {
+      const { firstName, lastName, dob, address } = req.body;
+      const validationError = validateUserFields(req.body);
+
+      if (validationError) {
+        res.status(400).json({ message: validationError });
+      } else {
+        await knex("users").insert({
+          firstName,
+          lastName,
+          dob,
+          address,
+        });
+        res.json({
+          message: `User ${firstName} ${lastName} with address ${address} and date of birth ${dob} created`,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "An error occurred while creating a user" });
+    }
+  };
+
+
+  const getUser = async (req, res) => {
+    try {
+      const data = await knex.select("*").from("users");
+      res.json({ data: data });
+    } catch (err) {
       console.log(err);
-    });
-};
+      res.status(500).json({ error: "An error occurred while fetching users" });
+    }
+  };
 
-module.exports = { createUser, getUser, updateUser, deleteUser };
+  const updateUser = async (req, res) => {
+    try {
+      await knex("users").where("id", req.params.id).update({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        dob: req.body.dob,
+        address: req.body.address,
+      });
+      res.send({ message: "User updated successfully" });
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while updating the user" });
+    }
+  };
+
+  const deleteUser = async (req, res) => {
+    try {
+      await knex("users").where("id", req.body.id).del();
+      res.json({ message: `User ${req.body.id} deleted.` });
+    } catch (err) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "An error occurred while deleting the user" });
+    }
+  };
+
+  module.exports = { createUser, getUser, updateUser, deleteUser };
--- db.js
+++ db.js
@@ -1,59 +1,43 @@
-// Import path module
-const path = require('path')
+const path = require("path");
 
-// Get the location of database.sqlite file
-const dbPath = path.resolve(__dirname, 'db/database.sqlite')
+// get the location of database.sqlite file
+const dbPath = path.resolve(__dirname, "db/database.sqlite");
 
-// Create connection to SQLite database
-const knex = require('knex')({
-  client: 'sqlite3',
+// create connection to sqlite database
+const knex = require("knex")({
+  client: "sqlite3",
   connection: {
     filename: dbPath,
   },
-  useNullAsDefault: true
-})
+  useNullAsDefault: true,
+});
 
-// Create a table in the database called "books"
 knex.schema
-  // Make sure no "books" table exists
-  // before trying to create new
-  .hasTable('users')
-    .then((exists) => {
-      if (!exists) {
-        // If no "books" table exists
-        // create new, with "id", "author", "title",
-        // "pubDate" and "rating" columns
-        // and use "id" as a primary identification
-        // and increment "id" with every new record (book)
-        return knex.schema.createTable('users', (table)  => {
-          table.increments('id').primary()
-          table.string('firstName')
-          table.string('lastName')
-          table.date('dob')
-          table.string('address')
+
+  .hasTable("users")
+  .then((exists) => {
+    if (!exists) {
+      return knex.schema
+        .createTable("users", (table) => {
+          table.increments("id").primary();
+          table.string("firstName");
+          table.string("lastName");
+          table.date("dob");
+          table.string("address");
         })
         .then(() => {
-          // Log success message
-          console.log('Table \'users\' created')
+          console.log("Table 'users' created");
         })
         .catch((error) => {
-          console.error(`There was an error creating table: ${error}`)
-        })
-      }
-    })
-    .then(() => {
-      // Log success message
-      console.log('done')
-    })
-    .catch((error) => {
-      console.error(`There was an error setting up the database: ${error}`)
-    })
+          console.error(`There was an error creating table: ${error}`);
+        });
+    }
+  })
+  .then(() => {
+    console.log("done");
+  })
+  .catch((error) => {
+    console.error(`There was an error setting up the database: ${error}`);
+  });
 
-// Just for debugging purposes:
-// Log all data in "books" table
-// knex.select('*').from('users')
-//   .then(data => console.log('data:', data))
-//   .catch(err => console.log(err))
-
-// Export the database
-module.exports = knex
+module.exports = knex;
